d58cfc0ce6c9e0ef48aed61a1e2bc95dd07b3f44
ZakyTm <zakaria.t149919@gmail.com>
2024-08-09 15:29:24 +0100

    initial Commit

---
+++ lib/ui/get_started.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context)
+        .size; // This size provide us total height and width of our screen
+
+    return Scaffold(
+      body: Container(
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        )),
+      ),
+    );
+  }
+}
